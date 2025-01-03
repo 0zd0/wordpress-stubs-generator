8f03af7c765925eaa7d8b4819aa3dc00082524c2
0zd0 <67220210+0zd0@users.noreply.github.com>
2025-01-03 12:09:39 +0000

    Fix finder type

--- src/GenerateStubsCommand.php
+++ src/GenerateStubsCommand.php
@@ -17,6 +17,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\OutputStyle;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\SplFileInfo;
 
 /**
